56cebda5c9b47a0b6a86093e2cc7d63ef9d6a73f
Tamas K. Stenczel <stenczelt@gmail.com>
2020-01-07 09:45:05 +0000

    update the structure and ignore build directories in .gitignore

--- projection_viewer/__init__.py
+++ projection_viewer/__init__.py
@@ -1,13 +1,14 @@
 import os
 
+import projection_viewer.backends
+import projection_viewer.frontend
 import projection_viewer.helpers
+import projection_viewer.processors
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-# fixme: this would be nicer with finding the package data
-# print get_data('resource1/foo.txt')
 
 
 def get_asset_folder():
+    # fixme: this would be nicer with finding the package data
+    # print get_data('resource1/foo.txt')
     return os.path.join(_ROOT, '../assets')
-
-
