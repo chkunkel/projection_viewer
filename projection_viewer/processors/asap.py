93beb76077ff8020ed02398ffae18758f26e50df
Tamas K. Stenczel <stenczelt@gmail.com>
2020-02-19 10:01:33 +0000

    use subprocess instead of os.system in asap.py processor

--- projection_viewer/processors/asap.py
+++ projection_viewer/processors/asap.py
@@ -1,5 +1,10 @@
-import os
-from subprocess import run
+from subprocess import run as r
+
+
+def run(cmd, *args, **kwargs):
+    cmd = [str(c) for c in cmd]
+    print('DEBUG: running command --- ', cmd)
+    r(cmd, *args, **kwargs)
 
 
 def abcd_download(fn, query_string):
@@ -34,24 +39,26 @@ def abcd_exec_query_and_run_asap(query_string, peratom=False, soap_n=6, soap_l=8
     abcd_download(abcd_fn, query_string)
 
     # exec ASAP gen_soap_descriptors.py
-    desc_executeable = "gen_soap_descriptors.py"
-    asap_soap_command = "{ex} -fxyz {fn} --l {l} --n {n} --g {g} --periodic True --rcut {cut} " \
-                        "--peratom {peratom}".format(fn=abcd_fn, ex=desc_executeable, peratom=peratom, g=soap_g,
-                                                     n=soap_n, l=soap_l, cut=soap_rcut)
-    os.system(asap_soap_command)
+    asap_soap_command = ["gen_soap_descriptors.py",
+                         "-fxyz={}".format(abcd_fn),
+                         "--l={}".format(soap_l),
+                         "--n={}".format(soap_n),
+                         "--g={}".format(soap_g),
+                         "--periodic={}".format(True),
+                         "--rcut={}".format(soap_rcut),
+                         "--peratom={}".format(peratom)]
+    run(asap_soap_command)
 
     # exec ASAP pca_minimal.py
-    pca_executeable = "pca_minimal.py"
     final_fn = "ASAP-pca-d4-new.xyz"
-    asap_pca_command = "{ex} --desc-key {key} --fxyz {in_fn} --output {fn} -d 4 --scale True " \
-                       "--peratom {peratom}".format(fn=final_fn, ex=pca_executeable, peratom=peratom, in_fn=foutput,
-                                                    key=desc_name)
-    os.system(asap_pca_command)
+    asap_pca_command = ["pca_minimal.py",
+                        "--desc-key={}".format(desc_name),
+                        "--fxyz={}".format(foutput),
+                        "--output={}".format(final_fn),
+                        "-d=4",
+                        "--scale={}".format(True),
+                        "--peratom={}".format(peratom)]
 
-    # the default commands, that are produced now
-    # python /home/tks32/work/ASAP/asap/gen_soap_descriptors.py -fxyz raw_abcd_data.xyz
-    #                       --l 8 --n 6 --g 0.5 --periodic True --rcut 4.5 --peratom True
-    # python /home/tks32/work/ASAP/asap/pca_minimal.py --output ASAP-pca-d4-new.xyz
-    #                       -d 4 --scale True --peratom True --desc-key SOAP-n6-l8-c4.5-g0.5 --fxyz ASAP-n6-l8-c4.5-g0.5.xyz
+    run(asap_pca_command)
 
     return final_fn
